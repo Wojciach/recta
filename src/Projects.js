99b837298ae76c22ee280880ad9391992e5d8928
Wojciach <wojciech.wyroslak@gmail.com>
2023-05-06 10:49:21 +0200

    added context provider and custom hook

--- src/Projects.js
+++ src/Projects.js
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, memo } from 'react';
+import { useImageIndex } from './ImageIndexContext';
+
 import "./Projects.scss";
 
 import photo1 from "./photos/annie-gray-WEWTGkPUVT0-unsplash_edit.jpg";
@@ -11,125 +13,121 @@ import photo7 from "./photos/ronnie-george-z11gbBo13ro-unsplash.jpg";
 import photo8 from "./photos/vlad-yuhimchuk-1S1mk3uDDDA-unsplash.jpg";
 import photo9 from "./photos/Main_slider.jpg";
 
-const Projects = () => {
+const photos = [photo1, photo2, photo3, photo4, photo5, photo6, photo7, photo8, photo9];
 
+const Projects = memo(() => {
+
+
+
+  const { currentImageIndex, setCurrentImageIndex } = useImageIndex();
+
+ // const [transformValue, setTransformValue] = useState("100");
+  const [transformValue, setTransformValue] = useState("100");
+
+  const style = {
+    transform: `translateX(${transformValue}%)`,
+    transition: 'transform 1s'
+  };
+
+  const updateTransform = (value) => {
+    setTransformValue(value);
+  };
+  
   
 
-        const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-
-        const photos = [photo1, photo2, photo3, photo4, photo5, photo6, photo7, photo8, photo9];
-        
-       /*
-          const images = [
-              'annie-gray-WEWTGkPUVT0-unsplash_edit.jpg',
-              'avel-chuklanov-a0BEHFMKqTE-unsplash.jpg',
-              'josh-olalde-X1P1_EDNnok-unsplash.jpg',
-              'Lipowa100-2.jpg',
-              'milivoj-kuhar-Te48TPzdcU8-unsplash_edit.jpg',
-              'Osiedle-domow-blizniaczych-przy-ul.-Czechoslowackiej-w-Lodzi-2-800x450.jpg',
-              'ronnie-george-z11gbBo13ro-unsplash.jpg',
-              'vlad-yuhimchuk-1S1mk3uDDDA-unsplash.jpg',
-              'Main_slider.jpg'
-
-            ];
-
-       const photos = [];
-
-        for (let i = 0; i < 9; i++) {
-            const photo = require(`./photos/${images[i]}`);
-            photos.push(photo);
-          }
-
-        */
-      
-        const previousImage = () => {
-          if (currentImageIndex >= 3) {
-            setCurrentImageIndex(currentImageIndex - 3);
-            slideRight();
-          }
-        };
-      
-        const nextImage = () => {
-            
-          if (currentImageIndex <= photos.length - 6) {
-            setCurrentImageIndex(currentImageIndex + 3);
-            slideLeft();
-          }
-        };
-
-    function slideLeft() {
-      console.log("left slided");
-     
-      document.querySelector(".photos > div").classList.add("slideLeft");
-      setTimeout( () => {document.querySelector(".photos > div").classList.remove("slideLeft");} , 250);
-
-
+  // Save currentImageIndex in session storage
+  useEffect(() => {
+    const storedIndex = sessionStorage.getItem('currentImageIndex');
+    if (storedIndex) {
+      setCurrentImageIndex(parseInt(storedIndex, 10));
     }
+  }, []);
 
-    function slideRight() {
-      console.log("RIGHT slided");
+  useEffect(() => {
+    sessionStorage.setItem('currentImageIndex', currentImageIndex);
+  }, [currentImageIndex]);
 
-      document.querySelector(".photos > div").classList.add("slideRight");
-      setTimeout( () => {document.querySelector(".photos > div").classList.remove("slideRight");} , 250);
-
-    }   
-    
-
-    useEffect(()=>{
+  const previousImage = () => {
+    if (currentImageIndex >= 3) {
+     
+      setCurrentImageIndex(currentImageIndex - 3);
+      slideRight();
       
-      if (currentImageIndex < 3) {
-        document.getElementsByClassName("slider")[0].classList.remove("trCenter");
-        document.getElementsByClassName("slider")[0].classList.remove("trRight");
-        document.getElementsByClassName("slider")[0].classList.add("trLeft");
-          
+    }
+  };
 
+  const nextImage = () => {
+    if (currentImageIndex <= photos.length - 6) {
+      
+      setCurrentImageIndex(currentImageIndex + 3);
+      slideLeft();
+      
+   
+    }
+  };
 
+  function slideLeft() {
+    console.log("left slided");
 
-      } else if ( currentImageIndex > 3 ) {
-        document.getElementsByClassName("slider")[0].classList.remove("trCenter");
-        document.getElementsByClassName("slider")[0].classList.remove("trLeft");
-        document.getElementsByClassName("slider")[0].classList.add("trRight");
-         
-         
+    document.querySelector(".photos > div").classList.add("slideLeft");
+    setTimeout( () => {document.querySelector(".photos > div").classList.remove("slideLeft");} , 250);
+  }
 
-          
-      } else if (currentImageIndex == 3 ) {
-        document.getElementsByClassName("slider")[0].classList.remove("trRight");
-        document.getElementsByClassName("slider")[0].classList.remove("trLeft");
-        document.getElementsByClassName("slider")[0].classList.add("trCenter");
-         
+  function slideRight() {
+    console.log("RIGHT slided");
 
+    document.querySelector(".photos > div").classList.add("slideRight");
+    setTimeout( () => {document.querySelector(".photos > div").classList.remove("slideRight");} , 250);
+  }
 
-      }
+  const moveSlider = () => {
 
-    })
+    const slider = document.getElementsByClassName("slider")[0];
 
+    if (currentImageIndex < 3) {
 
+     // slider.style.transform = "translateX(-100%)";
+      updateTransform("-100");
 
-    return (
-        <section id="projects">
-            <h2>Nasze Realizacje</h2>
+    } else if ( currentImageIndex === 3 ) { 
 
-            <div id="phSlider">
+     // slider.style.transform = "translateX(0)"; 
+      updateTransform("0");
+    } else if ( currentImageIndex > 3 ) {
 
-                <div className="sButton btnL" onClick={previousImage}></div>
+    // slider.style.transform = "translateX(100%)";
+      updateTransform("100");
+    }
+  }
+  
+  useEffect(() => {
+   
+      moveSlider();
+    
+  }, [currentImageIndex]);
 
-                <div className="photos">
-                    <div>
-                      {photos && photos.slice(currentImageIndex, currentImageIndex + 3).map((image, index) => (
-                      <img key={index} src={image} alt="" /> ) ) }
-                    </div>
-                </div>
+  return (
+    <section id="projects">
+      <h2>Nasze Realizacje</h2>
+  
+      <div id="phSlider">
+        <div className="sButton btnL" onClick={previousImage}></div>
+  
+        <div className="photos">
+          <div>
+            {photos && photos.slice(currentImageIndex, currentImageIndex + 3).map((image, index) => (
+            <img key={index} src={image} alt="" /> ) ) }
+          </div>
+        </div>
+  
+        <div className="sButton btnR" onClick={nextImage}></div>
+  
+        <div style={style} className="slider"></div>
+      </div>
+    </section>
+  );
 
-                <div className="sButton btnR" onClick={nextImage}></div>
-
-
-                <div className="slider trCenter"></div>
-            </div>
-
-        </section>
-    )
-}
+  
+})
 
 export default Projects;
