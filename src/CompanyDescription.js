70af39579b76c088aa287abff35d75ae050041bd
Wojciach <wojciech.wyroslak@gmail.com>
2023-10-24 12:06:20 +0200

    company description translated

--- src/CompanyDescription.js
+++ src/CompanyDescription.js
@@ -1,7 +1,7 @@
 import "./CompanyDescription.scss";
 
 import HaGallery from "./haGallery";
-import React, { useRef } from 'react';
+import React from 'react';
 
 import { ReactComponent as ContactIcon } from "./svg/Project_icon.svg";
 import { ReactComponent as ContactBacgroundSVG } from "./svg/RECTA_logo_granat.svg";
@@ -19,8 +19,8 @@ const CompanyDescription = () => {
     return(
         <section id="companyDescription">
 
-            <h2>Firma budowlana <b>Recta</b></h2>
-            <h2>dlaczego warto nam zaufać</h2>
+            <h2>Construction company <b>Recta</b></h2>
+            <h2>why you should trust us</h2>
 
             <div id="gOpis">
                 <div id="opis1" className="sec">
@@ -29,46 +29,30 @@ const CompanyDescription = () => {
                     </div>
                     <div>
                         <p>
-                            Jesteśmy doświadczoną i rzetelną firmą budowlaną, która 
-                            oferuje kompleksowe usługi budowlane oraz remontowe 
-                            dla klientów indywidualnych i biznesowych. Zawsze 
-                            stawiamy na jakość naszej pracy i solidność, dotrzymując 
-                            terminów i umów zawartych z klientami. Nasze usługi 
-                            obejmują m.in. budowę domów, budowę hal 
-                            przemysłowych, remonty mieszkań i domów, wykonanie 
-                            instalacji sanitarnych, elektrycznych oraz gazowych
+                            We are an experienced and reputable construction company that offers comprehensive construction and renovation services to individual and business clients. We always prioritize the quality of our work and reliability, adhering to deadlines and agreements made with our clients. Our services include, among others, building houses, constructing industrial halls, renovations of apartments and houses, and the installation of plumbing, electrical, and gas systems.
                         </p>
                     </div>
                 </div>
                 <div id="opis2" className="sec">
                 
                     <div>
-                        <p> Nasza firma zatrudnia wykwalifikowanych pracowników, 
-                            którzy posiadają odpowiednie kwalifikacje i 
-                            doświadczenie w branży budowlanej, aby zapewnić 
-                            najwyższą jakość usług dla naszych klientów.
+                        <p> 
+                            Our company employs qualified workers who have the necessary qualifications and experience in the construction industry to ensure the highest quality of services for our clients.
                         </p>
                         <p>
-                            Oferujemy naszym klientom dostępność i szybką reakcję 
-                            na ich potrzeby, a także dobrą komunikację w celu 
-                            zapewnienia pełnej satysfakcji. Stawiamy na najwyższą 
-                            jakość materiałów i wykonanie prac zgodnie z 
-                            najnowszymi standardami, aby zapewnić trwałość i 
-                            niezawodność naszych projektów.
+                            We offer our clients availability and a quick response to their needs, as well as good communication to ensure complete satisfaction. We prioritize the highest quality of materials and work execution according to the latest standards to ensure the durability and reliability of our projects.
                         </p>
                         <p>
-                            Zapraszamy do skorzystania z naszych usług 
-                            budowlanych i remontowych.
+                            We invite you to use our construction and renovation services.
                         </p>
                     </div>
 
                     <div id="contactLink">
                         <div className="contactBacground"> <ContactBacgroundSVG /> </div>
                         <div className="contactIcon"> <ContactIcon /> </div>
-                        <button onClick={() => scrollToElement(document.getElementById("contactForm"))}>Skontaktuj się</button> 
+                        <button onClick={() => scrollToElement(document.getElementById("contactForm"))}>Contact us</button> 
                         <p>
-                            Skontaktuj się z nami, aby poznać więcej szczegółów 
-                            lub uzyskać bezpłatną wycenę.
+                            Contact us to learn more details or to get a free estimate.
                         </p>
 
                     </div>
