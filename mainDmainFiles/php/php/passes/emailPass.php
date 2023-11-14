61d609fe69f4786cfee727cadc4c9e4b01822ea0
Wojciach <113638090+Wojciach@users.noreply.github.com>
2023-11-14 19:57:42 +0100

    Delete mainDmainFiles/php/php/passes directory

--- mainDmainFiles/php/php/passes/emailPass.php
+++
@@ -1,6 +0,0 @@
-<?php
-$eHost = 'smtp.hostinger.com';
-$eUserName ='formularz@recta.website';
-$ePassword ='%PRp?fvDvHJ4863';
-//verify_peer_name must be set to false for local dev and true for production
-$verify_peer = true;
